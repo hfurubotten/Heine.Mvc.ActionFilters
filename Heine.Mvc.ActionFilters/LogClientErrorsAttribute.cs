9086871c6dfeecaff837cd68dc5ea873ba67e788
Heine Furubotten <heine.furubotten@bouvet.no>
2017-11-15 13:52:57 +0100

    Added package for logging client errors. Only supports bad request for now

---
+++ Heine.Mvc.ActionFilters/LogClientErrorsAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http.Filters;
+using NLog;
+
+namespace Heine.Mvc.ActionFilters
+{
+    public class LogClientErrorsAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            base.OnActionExecuted(actionExecutedContext);
+
+            var statusCode = actionExecutedContext.Response.StatusCode;
+
+            if (statusCode == HttpStatusCode.BadRequest)
+            {
+                LogManager.GetCurrentClassLogger().Warn("Encountered a bad request from a client. \n" +
+                    "Request body: {0} \n" +
+                    "Response body: {1}", 
+                    GetBody(actionExecutedContext.Request.Content),
+                    GetBody(actionExecutedContext.Response.Content));
+            }
+        }
+
+
+        private static string GetBody(HttpContent content)
+        {
+            var requestStream = content.ReadAsStreamAsync().Result;
+            requestStream.Position = 0;
+            var streamReader = new StreamReader(requestStream, Encoding.UTF8);
+            var requestBody = streamReader.ReadToEnd();
+            return requestBody;
+        }
+    }
+}
