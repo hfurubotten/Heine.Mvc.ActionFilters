bb445a60a2c8ef37c1d3685ef7edbcfc52374bae
Sander Struijk <sander.struijk@bouvet.no>
2018-01-25 11:39:36 +0100

    Fixing Null Checking in RequestResponseCorrelationAttribute

--- Heine.Mvc.ActionFilters/Attributes/RequestResponseCorrelationAttribute.cs
+++ Heine.Mvc.ActionFilters/Attributes/RequestResponseCorrelationAttribute.cs
@@ -11,24 +11,31 @@ namespace Heine.Mvc.ActionFilters.Attributes
         private readonly string headerKey;
         private readonly Func<string> defaultValue;
 
+        public RequestResponseCorrelationAttribute() : this("X-CorrelationID", () => Guid.NewGuid().ToString()) { }
+
+        public RequestResponseCorrelationAttribute(string headerKey, string defaultValue) : this(headerKey, () => defaultValue) { }
+
         public RequestResponseCorrelationAttribute(string headerKey, Func<string> defaultValue)
         {
             this.headerKey = headerKey;
             this.defaultValue = defaultValue;
         }
 
-        public RequestResponseCorrelationAttribute(string headerKey, string defaultValue) : this(headerKey, () => defaultValue) { }
-
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
-            if (actionContext?.Request?.Headers?.Contains(headerKey) == true) return;
+            if (actionContext?.Request?.Headers == null) return;
+            if (actionContext.Request.Headers.Contains(headerKey)) return;
 
             actionContext.Request.Headers.Add(headerKey, defaultValue());
         }
 
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
-            if (actionExecutedContext?.Response?.Headers?.Contains(headerKey) == true) return;
+            if (actionExecutedContext?.Request?.Headers == null) return;
+            if (actionExecutedContext.Response?.Headers == null) return;
+
+            if (!actionExecutedContext.Request.Headers.Contains(headerKey)) return;
+            if (actionExecutedContext.Response.Headers.Contains(headerKey)) return;
 
             actionExecutedContext.Response.Headers.Add(headerKey, actionExecutedContext.Request.Headers.GetValues(headerKey));
         }
