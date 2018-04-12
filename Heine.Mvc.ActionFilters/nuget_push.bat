d00decea6d61e9d7172ba9bea2627706d67ec16d
Sander Struijk <sander.struijk@bouvet.no>
2018-04-12 16:03:24 +0200

    updated version numbers

--- Heine.Mvc.ActionFilters/nuget_push.bat
+++ Heine.Mvc.ActionFilters/nuget_push.bat
@@ -2,7 +2,7 @@
 :: "nuget setApiKey <personal_api_key>" => "The API Key 'personal_api_key>' was saved for the NuGet gallery (https://www.nuget.org) and the symbol server (https://nuget.smbsrc.net/)."
 
 SET packagename=Heine.Mvc.ActionFilters
-SET packageversion=3.0.0-beta07
+SET packageversion=3.0.0-beta08
 
 nuget push %packagename%.%packageversion%.nupkg -Source https://api.nuget.org/v3/index.json
 nuget push %packagename%.%packageversion%.symbols.nupkg -Source https://nuget.smbsrc.net
