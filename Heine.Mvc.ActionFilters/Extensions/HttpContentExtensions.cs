9ed0d5b9ffbaad076698c6587aa697cb3e177be7
Sander Struijk <sander.struijk@bouvet.no>
2018-04-16 13:05:10 +0200

    fixing bug where request body was not logged

--- Heine.Mvc.ActionFilters/Extensions/HttpContentExtensions.cs
+++ Heine.Mvc.ActionFilters/Extensions/HttpContentExtensions.cs
@@ -9,35 +9,44 @@ namespace Heine.Mvc.ActionFilters.Extensions
 {
     public static class HttpContentExtensions
     {
-        public static string ReadAsString(this HttpContent httpContent, bool format = true)
+        public static string ReadAsString(this HttpContent httpContent)
         {
-            if (httpContent == null) return string.Empty;
-
-            string content;
-
             try
             {
-                content = httpContent.ReadAsStringAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+                if (httpContent == null) return string.Empty;
 
-                if (format)
-                {
-                    switch (httpContent.Headers?.ContentType?.MediaType)
-                    {
-                        case "application/json":
-                            try { return JToken.Parse(content).ToString(Formatting.Indented); }
-                            catch { return content; }
-                        case "application/xml":
-                            try { return XDocument.Parse(content).ToString(); }
-                            catch { return content; }
-                    }
-                }
+                var stream = httpContent.ReadAsStreamAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+                stream.Position = 0;
+
+                // The stream returned by `ReadAsStreamAsync()` is the same stream used by `ReadAsStringAsync()`, 
+                // requiring us to reset the stream position before reading it in case it has already been consumed.
+
+                var content = httpContent.ReadAsStringAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+
+                // Resetting the stream again, just in case other consumers of the stream doesn't reset the stream position before trying read it.
+                stream.Position = 0;
+
+                return Format(content);
             }
             catch (Exception ex)
             {
-                content = $"Unable to read body of HTTP content:\n{string.Join("\n", ex.GetMessages().Select(message => $"- '{message}'"))}";
+                return $"Unable to read body of HTTP content:\n{string.Join("\n", ex.GetMessages().Select(message => $"- '{message}'"))}";
             }
 
-            return content;
+            string Format(string content)
+            {
+                switch (httpContent.Headers?.ContentType?.MediaType)
+                {
+                    case "application/json":
+                        try { return JToken.Parse(content).ToString(Formatting.Indented); }
+                        catch { return content; }
+                    case "application/xml":
+                        try { return XDocument.Parse(content).ToString(); }
+                        catch { return content; }
+                    default:
+                        return content;
+                }
+            }
         }
     }
 }
