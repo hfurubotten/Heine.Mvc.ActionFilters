3cbe59ce9dca4c03cef62d34e17e5e3f027a8856
Stian Sandve <stian@sandve.org>
2018-04-25 13:44:31 +0200

    Fixes issue #6

---
+++ Heine.Mvc.ActionFilters/Extensions/JsonExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace Heine.Mvc.ActionFilters.Extensions
+{
+    public static class JsonExtensions
+    {
+        public static bool IsNullOrEmpty(this JToken token) => 
+                   token == null ||
+                   token.Type == JTokenType.Array && !token.HasValues ||
+                   token.Type == JTokenType.Object && !token.HasValues ||
+                   token.Type == JTokenType.String && string.IsNullOrWhiteSpace(token.ToString()) ||
+                   token.Type == JTokenType.Null;
+    }
+}
