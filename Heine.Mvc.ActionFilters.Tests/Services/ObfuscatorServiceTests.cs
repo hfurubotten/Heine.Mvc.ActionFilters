251a91c1100dd9d8fa556168f63796f27ca4c467
Marius Vasshus <mvasshus@gmail.com>
2019-05-29 11:23:23 +0200

    Bugfix/exclude looping references (#20)
    * Implemented reference looping fix. Refactored and fixed other bugs as well.
    
    * Cleanup, added extra property with different type to test.

---
+++ Heine.Mvc.ActionFilters.Tests/Services/ObfuscatorServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using System.Linq;
+using System.Reflection;
+using FluentAssertions;
+using Heine.Mvc.ActionFilters.Services;
+using NUnit.Framework;
+
+namespace Heine.Mvc.ActionFilters.Tests.Services
+{
+    [TestFixture]
+    public class ObfuscatorServiceTests
+    {
+        private Assembly documentAssembly;
+
+        [SetUp]
+        public void Init()
+        {
+            // Arrange
+            documentAssembly = typeof(Document).Assembly;
+        }
+
+        [Test]
+        public void ObfuscatorService_ExpandDepthIsFive_ShouldReturnMultiplePaths()
+        {
+            // Act
+            var obfuscationService = new ObfuscaterService(5, documentAssembly);
+
+            var obfuscationGraph = obfuscationService.TypeObfuscationGraphs;
+            var documentGraph = obfuscationGraph[typeof(Document)];
+            var personGraph = obfuscationGraph[typeof(Person)];
+
+            // Assert
+            obfuscationGraph.Should().NotBeEmpty();
+            documentGraph.Should().NotBeEmpty();
+            personGraph.Should().NotBeEmpty();
+            documentGraph.Count.Should().Be(5);
+            documentGraph.FirstOrDefault(s => s.Contains("Content")).Should().NotBeNullOrEmpty();
+            documentGraph.FirstOrDefault(s => s.Contains("Owner.Mobil")).Should().NotBeNullOrEmpty();
+            documentGraph.FirstOrDefault(s => s.Contains("Owner.BankAccNo")).Should().NotBeNullOrEmpty();
+            documentGraph.FirstOrDefault(s => s.Contains("Persons[*].Item.Mobil")).Should().NotBeNullOrEmpty();
+            documentGraph.FirstOrDefault(s => s.Contains("Persons[*].Item.BankAccNo")).Should().NotBeNullOrEmpty();
+            personGraph.Count.Should().Be(4);
+            personGraph.FirstOrDefault(s => s.Contains("BankAccNo")).Should().NotBeNullOrEmpty();
+            personGraph.FirstOrDefault(s => s.Contains("Mobil")).Should().NotBeNullOrEmpty();
+            personGraph.FirstOrDefault(s => s.Contains("Documents[*].Item.Content")).Should().NotBeNullOrEmpty();
+            personGraph.FirstOrDefault(s => s.Contains("PrivateDocuments[*].Content")).Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void ObfuscatorService_ExpandDepthIsOne_ShouldReturnMultiplePaths()
+        {
+            // Act
+            var obfuscationService = new ObfuscaterService(1, documentAssembly);
+
+            var obfuscationGraph = obfuscationService.TypeObfuscationGraphs;
+            var documentGraph = obfuscationGraph[typeof(Document)];
+            var personGraph = obfuscationGraph[typeof(Person)];
+
+
+            // Assert
+            obfuscationGraph.Should().NotBeEmpty();
+            documentGraph.Should().NotBeEmpty();
+            personGraph.Should().NotBeEmpty();
+            documentGraph.Count.Should().Be(1);
+            documentGraph.FirstOrDefault(s => s.Contains("Content")).Should().NotBeNullOrEmpty();
+            personGraph.Count.Should().Be(2);
+            personGraph.FirstOrDefault(s => s.Contains("BankAccNo")).Should().NotBeNullOrEmpty();
+            personGraph.FirstOrDefault(s => s.Contains("Mobil")).Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void ObfuscatorService_ExpandDepthIsZero_ShouldNotReturnAnyPaths()
+        {
+            // Act
+            var obfuscationService = new ObfuscaterService(0, documentAssembly);
+
+            var obfuscationGraph = obfuscationService.TypeObfuscationGraphs;
+
+            // Assert
+            obfuscationGraph.Should().BeEmpty();
+        }
+    }
+}
---
+++ Heine.Mvc.ActionFilters.Tests/Services/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Heine.Mvc.ActionFilters.Tests.Services
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Document> Documents { get; set; }
+        public Document[] PrivateDocuments { get; set; }
+        [Obfuscation]
+        public string Mobil { get; set; }
+        [Obfuscation]
+        public string BankAccNo { get; set; }
+    }
+}
