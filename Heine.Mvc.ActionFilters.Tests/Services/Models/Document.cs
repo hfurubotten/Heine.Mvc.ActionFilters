8d0e5e03a6572e54466f674ada4768cb8eef70ee
Jan Marius Vasshus <marius.vasshus@bouvet.no>
2019-06-03 15:14:31 +0200

    Implemented fix in expand for generic collections.

--- Heine.Mvc.ActionFilters.Tests/Services/Models/Document.cs
+++ Heine.Mvc.ActionFilters.Tests/Services/Models/Document.cs
@@ -5,11 +5,12 @@ namespace Heine.Mvc.ActionFilters.Tests.Services.Models
 {
     public class Document
     {
+        public ICollection<Document> Children { get; set; }
+        public List<Person> Persons { get; set; }
         public string Name { get; set; }
         [Obfuscation]
         public string Content { get; set; }
         public string Type { get; set; }
-        public List<Person> Persons { get; set; }
         public Person Owner { get; set; }
         public Document Parent { get; set; }
     }
